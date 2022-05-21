a9253aed212a96606bc7a00eb38d4c30bdd0794c
The Charged Ion <104731684+The-Charged-Ion@users.noreply.github.com>
2022-05-21 21:38:54 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,12 @@
+# Mathematical Operators:
+#     Addition - (+)
+#     Multiplication - (*)
+#     Subtraction - (-)
+#     Division - (/)
+
+# Strings are Anything inside Double Quotes, Single Quotes or Triple Quotes
+# (print) is used for printing anything in the terminal/in python.
+
+print(int(input("Number One: ")) * int(input("Number Two: ")))
+
+
